850ec32273cf36af518d96b78d68059731059410
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-08-28 13:48:27 -0500

    ..

--- index.php
+++ index.php
@@ -1,46 +1,41 @@
 <?php
-// Definición de la clase base 'Animal'
-class Animal {
-    public $nombre; // Propiedad para almacenar el nombre del animal
-    
-    // Constructor de la clase 'Animal'
-    public function __construct($nombre) {
-        $this->nombre = $nombre; // Establecer el nombre del animal al crear un objeto
-    }
-    
-    // Método para emitir un sonido genérico
-    public function emitirSonido() {
-        return "Haciendo un sonido genérico"; // Mensaje predeterminado
-    }
-}
+// Declaración de la clase base 'Vehiculo'
+class Vehiculo {
+    // Propiedades públicas de la clase
+    public $motor = false;  // Indica si el motor está encendido o apagado
+    public $marca;          // Marca del vehículo
+    public $color;          // Color del vehículo
 
-// Definición de la clase 'Perro', que hereda de 'Animal'
-class Perro extends Animal {
-    // Sobrescribir el método 'emitirSonido' para perros
-    public function emitirSonido() {
-        return "Woof woof!"; // Sonido específico para perros
+    // Método para mostrar el estado del motor
+    public function estado() {
+        if ($this->motor) {
+            echo "El motor está encendido<br>";
+        } else {
+            echo "El motor está apagado<br>";
+        }
     }
-}
 
-// Definición de la clase 'Gato', que hereda de 'Animal'
-class Gato extends Animal {
-    // Sobrescribir el método 'emitirSonido' para gatos
-    public function emitirSonido() {
-        return "Miau!"; // Sonido específico para gatos
+    // Método para encender el motor
+    public function encender() {
+        if ($this->motor) {
+            echo "Cuidado, el motor ya está encendido<br>";
+        } else {
+            $this->motor = true;  // Cambia el estado del motor a encendido
+            echo "El motor ahora está encendido<br>";
+            $this->motor = true;  // ¡Este duplicado parece ser un error y podría eliminarse!
+        }
     }
 }
 
-// Crear un objeto de la clase 'Perro' con nombre 'Fido'
-$perro = new Perro("FIRULAIS");
+// Declaración de la clase 'Moto' que hereda de la clase 'Vehiculo'
+class Moto extends Vehiculo {
+    // No contiene ningún método ni propiedad adicional, hereda las de 'Vehiculo'
+}
 
-// Crear un objeto de la clase 'Gato' con nombre 'Whiskers'
-$gato = new Gato("MICHI");
+// Creación de una instancia de la clase 'Moto'
+$moto = new Moto();
 
-// Mostrar el nombre y el sonido del perro
-echo $perro->nombre . ": " . $perro->emitirSonido() . "<br>";
-
-// Mostrar el nombre y el sonido del gato
-echo $gato->nombre . ": " . $gato->emitirSonido();
+// Llamada al método 'estado()' de la instancia de 'Moto'
+$moto->estado();
 
 ?>
-
